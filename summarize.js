94f71009c68053322dbe7e3ed130fb82927d1ac4
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2024-05-02 09:11:32 +0530

    yes

---
+++ summarize.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+// This is the function where the call to the API is made. Returns the summarized text as a string.
+
+async function summarizeText(text) {
+
+  // INSERT CODE SNIPPET FROM POSTMAN BELOW
+  const axios = require('axios');
+  let data = JSON.stringify({
+    "inputs": text,  // <-- use the text passed into the function
+    "parameters": {
+      "max_length": 100,
+      "min_length": 30
+    }
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': 'Bearer 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'
+        // 'Authorization': 'Bearer' + process.env['ACCESS_TOKEN']
+    },
+    data : data
+  };
+
+  
+    try {
+      const response = await axios.request(config);
+      return response.data[0].summary_text;
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+  
+}
+
+// Allows for summarizeText() to be called outside of this file
+
+module.exports = summarizeText;
